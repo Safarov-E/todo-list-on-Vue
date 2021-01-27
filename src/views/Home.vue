b985c1b6d2adfcaf9082a34aa1aefdab1e6755ba
safarov-e <elkhan.safarov10@yandex.com>
2021-01-27 15:05:43 +0500

    Мультистраничность с Vue Router

---
+++ src/views/Home.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+        <h2>Home page</h2>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repudiandae ex temporibus consequuntur tempora nobis numquam iste. Assumenda repudiandae vero eligendi aspernatur fugit, culpa, nesciunt laudantium doloremque sed consequatur, minima impedit.</p>
+        <router-link to="/todos">Todos</router-link>
+    </div>
+</template>
