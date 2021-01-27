1e371efb1d928eef17131b0542b6149164ce265d
safarov-e <elkhan.safarov10@yandex.com>
2021-01-27 20:48:29 +0500

    Computed свойства

--- src/views/Todos.vue
+++ src/views/Todos.vue
@@ -6,12 +6,17 @@
     <AddTodo 
       @add-todo="addTodo"
     />
+    <select v-model="filter">
+      <option value="all">All</option>
+      <option value="completed">Completed</option>
+      <option value="not-completed">Not Completed</option>
+    </select>
     <hr>
     <Loader v-if="loading" />
     <template v-else>
       <TodoList 
-        v-if="todos.length"
-        v-bind:todos="todos"
+        v-if="filteredTodos.length"
+        v-bind:todos="filteredTodos"
         @remove-todo="removeTodo"
       />
       <p v-else>No todos!</p>
@@ -28,7 +33,8 @@ export default {
   data() {
     return {
       todos: [],
-      loading: true
+      loading: true,
+      filter: 'all'
     }
   },
   mounted() {
@@ -40,6 +46,19 @@ export default {
           this.loading = false
         }, 1000)
       })
+  },
+  computed: {
+    filteredTodos() {
+      if(this.filter === 'all') {
+        return this.todos
+      }
+      if(this.filter === 'completed') {
+        return this.todos.filter(t => t.completed)
+      }
+      if(this.filter === 'not-completed') {
+        return this.todos.filter(t => !t.completed)
+      }
+    }
   },
   methods: {
     removeTodo(id) {
