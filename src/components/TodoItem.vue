6de5c758b22e121dc36ec03b3f96bf2dd52674ee
safarov-e <elkhan.safarov10@yandex.com>
2021-01-27 11:33:36 +0500

    Работа со стилями

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,6 +1,11 @@
 <template>
     <li>
-        {{ todo.title }}
+        <span>
+            <input type="checkbox" />
+            <strong>{{todo.id}}</strong>
+            {{todo.title}}
+        </span>
+        <button class="rm">&times;</button>
     </li>
 </template>
 <script>
@@ -13,3 +18,24 @@ export default {
     }
 }
 </script>
+<style scoped>
+    li {
+        border: 1px solid #ccc;
+        display: flex;
+        justify-content: space-between;
+        padding: .5rem 2rem;
+        margin-bottom: 1rem;
+    }
+    .rm {
+        background: red;
+        color: #fff;
+        border-radius: 50%;
+        font-weight: bold;
+    }
+    input {
+        margin-right: 1rem;
+    }
+    .done {
+        text-decoration: line-through;
+    }
+</style>
